41ca6640e64bc2c8d92b6ced7dc561c3f2ea4a66
weak <weak@fraglab.at>
2017-08-11 13:37:01 +0200

    watchOS test

---
+++ Swell_watchOS/Swell_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Swell_watchOS.h
+//  Swell_watchOS
+//
+//  Created by weak on 11/08/2017.
+//  Copyright © 2017 Minute Apps LLC. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Swell_watchOS.
+FOUNDATION_EXPORT double Swell_watchOSVersionNumber;
+
+//! Project version string for Swell_watchOS.
+FOUNDATION_EXPORT const unsigned char Swell_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swell_watchOS/PublicHeader.h>
+
+
